2cc77dbc59e55a85bd8b454357b8e1c50d04bb58
Ugochukwu Ekeh <ekeh.uj@gmail.com>
2024-07-13 01:53:41 +1000

    Created PostFactorty and Post Seeder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
+    {   
         $posts = Post::all();
         return view('Posts.index', compact('posts'));
     }
